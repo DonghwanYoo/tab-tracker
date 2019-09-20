e6574c1f66ffcbfb545380a2d01dd889d996d19e
Donghwan Yoo <donghwan.yoo@parksystems.com>
2019-09-20 13:57:47 +0900

    setting up the registration page

--- server/src/app.js
+++ server/src/app.js
@@ -9,9 +9,9 @@ app.use(morgan('combined')); // for logging
 app.use(bodyParser());
 app.use(cors());
 
-app.get('/status', (req, res) => {
+app.post('/register', (req, res) => {
     res.send({
-        message: 'hello world'
+        message: `Hello ${req.body.email} Your user was registered! Have fun!`
     })
 })
 
